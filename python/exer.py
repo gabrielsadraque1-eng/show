1ad089512ddcc7480ea5eb6eb5d5785efe38e6ae
gabrielsadraque1-eng <gabrielsadraque1@gmail.com>
2026-09-24 08:43:46 -0300

    adiciona páginas HTML para artistas da 30PRAUM e tabela de shows atualização do projeto

---
+++ python/exer.py
@@ -0,0 +1,13 @@
+nome=str(input("digite seu nome : "))
+print("ola",nome ,"gostoso")
+idade=int(input("digite sua idade : "))
+if idade>=18:
+    print("voce ja pode ser preso")
+else:
+    print("voce deve esperar para ser preso")
+
+num1=int(input("digite um numero :"))
+if num1%2==0:
+    print("o numero",num1,"e par")
+if num1%2==1:
+    print("o numero",num1,"e impar")
